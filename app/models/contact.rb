fd2aafb2e2f3f1c1e78dd558baf610ba0f557b5d
BenRuns <benthomaspatterson@gmail.com>
2014-07-31 13:56:12 -0700

    more tests & validation on the contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,18 @@
 class Contact < ActiveRecord::Base
 
 	validates :first_name, presence:true,
-						length: {minimum: 1
+						length: {minimum: 1,  maximum: 35
 						}
+	validates :middle_name, allow_nil:true,
+						length: {minimum: 1,  maximum: 35
+						}
+	validates :last_name, allow_nil:true,
+						length: {minimum: 1,  maximum: 35
+						}
+
+	validates :email, allow_nil:true ,
+					  format: {
+                      with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]+\Z/
+                      }
+
 end
--- spec/models/contacts_spec.rb
+++ spec/models/contacts_spec.rb
@@ -1,17 +1,64 @@
 require 'spec_helper'
 
+
 describe Contact do
+ 
+	 before do
+	 	@contact = Contact.new
+	 end
+
   it "should not save without content" do 
+  	 
   	@contact = Contact.new
   	expect(@contact.save).to eq(false)
 
   end
-  
-  it "should save with first name and number " do 
-  	@contact = Contact.new
+
+  it "should save with a first name and number " do 
+   
   	@contact.first_name = "Fake"
   	@contact.phone_number = "6765443333"
   	expect(@contact.save).to eq(true)
   end
+
+  it "should not save an invalid email" do 
+  	 
+  	@contact.first_name = "Fake"
+  	@contact.email = "6765443333"
+  	expect(@contact.save).to eq(false)
+  end
+
+  it "should save  a valid email" do 
+  	@contact.first_name = "Fake"
+  	@contact.email = "fake@email.com"
+  	expect(@contact.save).to eq(true)
+
+  end
+
+
+  it "should not save a first name over 35 characters" do 
+  	forty_a = "a" * 40
+  	@contact.first_name = forty_a
+  	expect(@contact.save).to eq(false)
+  end
+
+  it "should not save a last name over 35 characters" do 
+  	forty_a = "d" * 40
+  	@contact.first_name = "Ben"
+  	@contact.last_name = forty_a
+  	expect(@contact.save).to eq(false)
+  end
+
+  it "should not save a middle name over 35 characters" do 
+  	forty_a = "e" * 40
+  	@contact.first_name = "Ben"
+  	@contact.middle_name = forty_a
+  	expect(@contact.save).to eq(false)
+  end
+
+
+
+
+
 
 end

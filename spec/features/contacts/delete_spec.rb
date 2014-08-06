dc593925103ef3d1a273ddbb75cc22d1e34db745
BenRuns <benthomaspatterson@gmail.com>
2014-08-06 15:20:24 -0700

    I forgot what I added

--- spec/features/contacts/delete_spec.rb
+++ spec/features/contacts/delete_spec.rb
@@ -2,21 +2,21 @@
 require 'spec_helper'
 
 
-
 feature "User deletes a contact" do
-	let!(:contact) {create(:contact)}
-	 
+
+
 
 
 	scenario  "user successfully deletes contact " do
-		id = contact.id
-		contact.first_name = "Steve"
+		
 
-		visit contacts_path
-		within("#results-entry-#{id}") do 
-			click_link "Edit"
 
-		end
+		Capybara.current_driver = :selenium
+       @contact = Contact.new( :first_name => "Fake",
+   :last_name => "FakeLname", :email => "khgk@hgkg.ney", :id => "5")
+       @contact.save
+
+		visit contact_path(@contact)
 
 		expect(page).to have_content("Editing")
 		click_button "Delete"
@@ -25,39 +25,5 @@ feature "User deletes a contact" do
 		expect(page).to_not have_content("#results-entry-#{id}")
 	end
 
-	scenario  "user successfully stops from deleting a contact " do
-		id = contact.id
-		contact.first_name = "Steve"
-
-		visit contacts_path
-		within("#results-entry-#{id}") do 
-			click_link "Edit"
-
-		end
-
-		expect(page).to have_content("Editing")
-		click_button "Delete"
-		expect(page).to have_content("Are you sure you want to delete this contact?")
-		click_link "No"
-		expect(page).to have_content("Contact has not been deleted")
-		expect(page).to have_content("#results-entry-#{id}")
-	end
-
-	scenario  "user returns to the home page before deleting contact" do
-		id = contact.id
-		contact.first_name = "Steve"
-
-		visit contacts_path
-		within("#results-entry-#{id}") do 
-			click_link "Edit"
-
-		end
-
-		expect(page).to have_content("Editing")
-		click_button "Delete"
-		expect(page).to have_content("Are you sure you want to delete this contact?")
-		visit contacts_path
-		expect(page).to have_content("#results-entry-#{id}")
-	end
 
 end
